04a8fe74e81d6c7a10a68a3706472b6edec3e39f
Virender Parasariya <parasariya.1@iitj.ac.in>
2025-01-23 00:45:20 +0530

    making some changes to package.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,6 +30,13 @@ app.use(cors(corsOptions)); // Apply the CORS configuration
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
